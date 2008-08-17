3172aff53feecdaa9ddc2aab6d0bc2bf890f9411
Allister Beharry <allisterbeharry@116802.no-reply.drupal.org>
2008-08-17 18:47:56 +0000

    Added SIOC rendering for story node - no comments.

--- views-view-rdf.tpl.php
+++ views-view-rdf.tpl.php
@@ -139,8 +139,10 @@ function rdf_foaf_xml_render($view) {
  * @return none
  */
 function rdf_sioc_xml_render($view) {
+	//var_dump($view);
+	//module_invoke_all('exit');
+  //exit;
 	global $base_url;
-	$users = array();
 	$xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 	$xml .= '<!-- generator="Drupal Views_Datasource.Module" -->'."\n";
   $xml .= "<rdf:RDF\r\n";
@@ -185,12 +187,9 @@ function rdf_sioc_xml_render($view) {
       else
         drupal_set_message('The email field must be present.', 'error');
       return;
-    }
-        
-  }
-  if ($view->base_table == 'users') {
+    } 
     $xml .= "<foaf:Document rdf:about=\"".url($view->name, array('absolute'=>true))."\">\n";
-    $xml .= "  <dc:title>SIOC profiles for: ".variable_get('site_name', 'drupal')."</dc:title>\n";
+    $xml .= "  <dc:title>SIOC user profiles for: ".variable_get('site_name', 'drupal')."</dc:title>\n";
     $xml .= "  <dc:description>\n";
     $xml .= "    A User is an online account of a member of an online community. 
      It is connected to Items and Posts that a User creates or edits, 
@@ -207,46 +206,109 @@ function rdf_sioc_xml_render($view) {
     //$xml .= "<foaf:primaryTopic rdf:resource=\"####user_name###\"/>\n";
     $xml .= "  <admin:generatorAgent rdf:resource=\"http://drupal.org/project/views_datasource\"/>\n";
     $xml .= "</foaf:Document>\n";
+    foreach($view->result as $node) $xml .= rdf_sioc_xml_user_render($node);     
   }
-  foreach($view->result as $node) {	
-  	foreach($node as $field_label=>$field_value) {
-      $label = views_rdf_strip_illegal_chars($field_label);
-      $value = views_xml_strip_illegal_chars(views_xml_is_date($field_value));
-      if (is_null($value) || ($value === '')) continue;
-      if (strtotime($value))
-        $value = date(DATE_ISO8601, strtotime($value));
-      if ((strtolower($label) == 'id') || (strtolower($label) == 'uid')) {
-      	$uid = $value;    	
-      }
-  	  if ((strtolower($label) == 'name') || (strtolower($label) == 'users_name')) {
-        $user_name = $value;      
-      }
-  	  if ((strtolower($label) == 'email') || (strtolower($label) == 'users_mail')) {
-        $user_email = $value;      
-      }            
-  	}
-  	if (empty($user_name)) continue;
-    $xml .="<foaf:Person rdf:ID=\"$user_name\" rdf:about=\"".url('user/'.$uid, array('absolute'=>true))."\">\n";
-    $xml .="  <foaf:name>$user_name</foaf:name>\n";
-    $xml .="  <foaf:mbox_sha1sum>".md5('mailto:'.$user_email)."</foaf:mbox_sha1sum>\n";
-    $xml .="  <foaf:holdsAccount>\n";
-    $xml .="    <sioc:User rdf:about=\"".url('user/'.$uid, array('absolute'=>true))."\">\n";
-    $xml .="      <sioc:name>$user_name</sioc:name>\n";
-    $xml .="      <sioc:email rdf:resource=\"$user_email\"/>\n";
-    $xml .="      <sioc:email_sha1>".md5('mailto:'.$user_email)."</sioc:email_sha1>\n";
-    $roles = array();
-    $roles_query = db_query("SELECT r.name AS name, r.rid AS rid FROM {users_roles} ur, {role} r WHERE ur.uid = %d AND ur.rid = r.rid", $uid);
-    while ($role = db_fetch_object($roles_query))
-      $roles[$role->rid] = $role->name;
-    if (count($roles) > 0) {
-      $xml .="      <sioc:has_function>\n";
-      foreach($roles as $rid=>$name)
-        $xml .="        <sioc:Role><rdfs:label><![CDATA[$name]]></rdfs:label></sioc:Role>\n";
-      $xml .="      </sioc:has_function>\n"; 	  
-    }             
-    $xml .="    </sioc:User>\n";
-    $xml .="  </foaf:holdsAccount>\n";
-    $xml .="</foaf:Person>\n";	
+  if ($view->base_table == 'node') {
+    $has_nid = false;
+    $has_type = false;
+    $has_created = false;
+    $has_changed = false;
+    $has_last_updated = false;
+    $has_title = false;
+    $has_body = false;
+    $has_uid = false;
+    foreach($view->field as $field) {
+      //if (($field->field_alias == 'uid') && ($field['options']['field'] ==  'uid'))
+      if ($field->options['field'] ==  'nid') 
+        $has_nid = true;
+      if ($field->options['field'] ==  'type') 
+        $has_type = true;
+      if ($field->options['field'] ==  'created') 
+        $has_created = true;
+      if ($field->options['field'] ==  'changed') 
+        $has_changed = true;
+      if ($field->options['field'] ==  'last_updated') 
+        $has_last_updated = true;
+      if ($field->options['field'] ==  'title') 
+        $has_title = true;
+      if ($field->options['field'] == 'body')
+        $has_body = true;
+      if ($field->options['field'] ==  'uid') 
+        $has_uid = true;        
+    }
+    if (!$has_nid) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Nid field must be present.</b>');
+      else
+        drupal_set_message('The Node: Nid field must be present.', 'error');
+      return;
+    }
+    if (!$has_type) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Type field must be present.</b>');
+      else
+        drupal_set_message('The Node: Type field must be present.', 'error');
+      return;
+    }
+    if (!$has_created) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Post date field must be present.</b>');
+      else
+        drupal_set_message('The Node: Post date field must be present.', 'error');
+      return;
+    }
+    if (!$has_changed) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Updated date field must be present.</b>');
+      else
+        drupal_set_message('The Node: Updated date field must be present.', 'error');
+      return;
+    }
+    if (!$has_last_updated) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Updated/commented date field must be present.</b>');
+      else
+        drupal_set_message('The Node: Updated/commented date field must be present.', 'error');
+      return;
+    }
+    if (!$has_title) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Title field must be present.</b>');
+      else
+        drupal_set_message('The Node: Title field must be present.', 'error');
+      return;
+    }
+    if (!$has_body) {
+      if ($view->override_path)
+        print ('<b style="color:red">The Node: Body field must be present.</b>');
+      else
+        drupal_set_message('The Node: Body field must be present.', 'error');
+      return;
+    }    
+    if (!$has_uid) {
+      if ($view->override_path)
+        print ('<b style="color:red">The User: Uid field must be present.</b>');
+      else
+        drupal_set_message('The User: Uid field must be present.', 'error');
+      return;
+    }
+    $users = array();
+    $nodes = array();
+    $xml .= "<foaf:Document rdf:about=\"".url($view->name, array('absolute'=>true))."\">\n";
+    $xml .= "  <dc:title>SIOC profile for: ".variable_get('site_name', 'drupal')."</dc:title>\n";
+    $xml .= "  <dc:description>\n";
+    $xml .= "    A Post is an article or message posted by a User to a Forum or Site. A series of Posts 
+    may be threaded if they share a common subject and are connected by reply or 
+    by date relationships. Posts will have content and may also have attached 
+    files, which can be edited or deleted by the Moderator of the Forum or Site that 
+    contains the Post.\n";
+    $xml .= "  </dc:description>\n";
+    //$xml .= "  <foaf:primaryTopic rdf:resource=\"$node_url\"/>\n";
+    $xml .= "  <admin:generatorAgent rdf:resource=\"http://drupal.org/project/views_datasource\"/>\n";
+    $xml .= "</foaf:Document>\n";
+    foreach($view->result as $node) rdf_sioc_xml_node_render($node, &$users, &$nodes);
+    foreach ($users as $user_xml) $xml .= $user_xml;
+    foreach ($nodes as $node_xml) $xml .= $node_xml;
   }
   $xml.="</rdf:RDF>\n";
   if ($view->override_path) //inside live preview 
@@ -254,10 +316,110 @@ function rdf_sioc_xml_render($view) {
   else {  
     drupal_set_header('Content-Type: application/rdf+xml');
     print $xml;
-    //var_dump($view);
     module_invoke_all('exit');
     exit;
-  }	
-  
+  }
 }
-  
+
+function rdf_sioc_xml_user_render($node, $uid=null, $user_name=null, $user_email=null) {
+  if (func_num_args() == 1) {
+	  foreach($node as $field_label=>$field_value) {
+      $label = views_rdf_strip_illegal_chars($field_label);
+      $value = views_xml_strip_illegal_chars(views_xml_is_date($field_value));
+      if (is_null($value) || ($value === '')) continue;
+      if (strtotime($value))
+        $value = date(DATE_ISO8601, strtotime($value));
+      if ((strtolower($label) == 'id') || (strtolower($label) == 'uid')) {
+        $uid = $value;      
+      }
+      if ((strtolower($label) == 'name') || (strtolower($label) == 'users_name')) {
+        $user_name = $value;      
+      }
+      if ((strtolower($label) == 'email') || (strtolower($label) == 'users_mail')) {
+        $user_email = $value;      
+      }            
+    }
+    if (empty($user_name)) return;
+  }
+  $xml .="<foaf:Person rdf:about=\"".url('user/'.$uid, array('absolute'=>true))."\">\n";
+  $xml .="  <foaf:name>$user_name</foaf:name>\n";
+  $xml .="  <foaf:mbox_sha1sum>".md5('mailto:'.$user_email)."</foaf:mbox_sha1sum>\n";
+  $xml .="  <foaf:holdsAccount>\n";
+  $xml .="    <sioc:User rdf:nodeID=\"$uid\">\n";
+  $xml .="      <sioc:name>$user_name</sioc:name>\n";
+  $xml .="      <sioc:email rdf:resource=\"mailto:$user_email\"/>\n";
+  $xml .="      <sioc:email_sha1>".md5('mailto:'.$user_email)."</sioc:email_sha1>\n";
+  $xml .="      <sioc:link rdf:resource=\"".url('user/'.$uid, array('absolute'=>true))."\" rdfs:label=\"$user_name\"/>\n";
+  $roles = array();
+  $roles_query = db_query("SELECT r.name AS name, r.rid AS rid FROM {users_roles} ur, {role} r WHERE ur.uid = %d AND ur.rid = r.rid", $uid);
+  while ($role = db_fetch_object($roles_query)) $roles[$role->rid] = $role->name;
+  if (count($roles) > 0) {
+    $xml .="      <sioc:has_function>\n";
+    foreach($roles as $rid=>$name)
+    $xml .="        <sioc:Role><rdfs:label><![CDATA[$name]]></rdfs:label></sioc:Role>\n";
+    $xml .="      </sioc:has_function>\n";    
+  }             
+  $xml .="    </sioc:User>\n";
+  $xml .="  </foaf:holdsAccount>\n";
+  $xml .="</foaf:Person>\n";  
+	return $xml;
+}
+
+function rdf_sioc_xml_node_render($node, &$users=null, &$nodes = null) {
+	global $base_url;
+  //i
+  foreach($node as $field_label=>$field_value) {
+    $label = views_rdf_strip_illegal_chars($field_label);
+    $value = views_rdf_strip_illegal_chars(views_rdf_is_date($field_value));
+    if (is_null($value) || ($value === '')) continue;
+    if (strtotime($value))
+      $value = date(DATE_ISO8601, strtotime($value));
+    if ((strtolower($label) == 'id') || (strtolower($label) == 'nid')) {
+      $nid = $value;      
+    }
+    if ((strtolower($label) == 'title') || (strtolower($label) == 'node_title')) {
+      $title = $value;      
+    }
+    if ((strtolower($label) == 'body') || (strtolower($label) == 'node_revisions_body')) {
+      $body = $value;      
+    }
+    if ((strtolower($label) == 'type') || (strtolower($label) == 'node_type')) {
+      $type = $value;      
+    }
+    if ((strtolower($label) == 'created') || (strtolower($label) == 'node_created')) {
+      $created = $value;
+    }
+    if ((strtolower($label) == 'changed') || (strtolower($label) == 'node_changed')) {
+      $changed = $value;      
+    }            
+    if ((strtolower($label) == 'last_updated') || (strtolower($label) == 'node_comment_statistics_last_updated')) {
+      $last_updated = $value;      
+    }
+    if ((strtolower($label) == 'uid') || (strtolower($label) == 'users_uid')) {
+      $uid = $value;      
+    }    
+  }
+  $user = user_load($uid);  
+  if (!array_key_exists($uid, $users)) $users[$uid] = rdf_sioc_xml_user_render(null, $uid, $user->name, $user->mail);
+  if (!array_key_exists($nid, $nodes)) {
+    if ($type == 'story') $nodes[$nid] = rdf_sioc_xml_story_render($xml, $nid, $title, $type, $created, $changed, $last_updated, $uid, $body);  	
+  }
+  //$xml = '';
+  //var_dump($nodes);
+  //var_dump($users);
+  //module_invoke_all('exit');
+  return;
+}
+
+function rdf_sioc_xml_story_render($xml, $nid, $title, $type, $created, $changed, $last_updated, $uid, $body) {
+	$node_url = url($nid, array('absolute'=>true));
+  $xml .= "<sioc:Post rdf:about=\"$node_url\">\n";
+  $xml .= "  <dc:title>$title</dc:title>\n";
+  $xml .= "  <sioc:content><![CDATA[$body]]></sioc:content>\n";
+  $xml .= "  <dc:created>".date(DATE_ISO8601, $created)."</dc:created>\n";
+  $xml .= "  <dc:modified>".date(DATE_ISO8601, $changed)."</dc:modified>\n";
+  $xml .= "  <sioc:link rdf:resource=\"$node_url\" rdfs:label=\"$title\" />\n";
+  $xml .= "  <sioc:has_creator rdf:nodeID=\"$uid\"/>\n";
+  $xml .= "</sioc:Post>\n";
+	return $xml;
+}	 
